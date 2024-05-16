b6cda0f380bc1ba643336e0de36d8ce86a66bf6d
kyungseok.lee <lks574@naver.com>
2024-05-16 17:15:32 +0900

    feat: AppMain 추가

--- Modules/Feature/Home/Package.swift
+++ Modules/Feature/Home/Package.swift
@@ -5,14 +5,24 @@ import PackageDescription
 
 let package = Package(
   name: "Home",
+  platforms: [
+    .iOS(.v17),
+  ],
   products: [
     .library(
       name: "Home",
       targets: ["Home"]),
   ],
+  dependencies: [
+    .package(path: "../../Core/Architecture"),
+  ],
   targets: [
     .target(
-      name: "Home"),
+      name: "Home",
+      dependencies: [
+        "Architecture",
+      ]
+    ),
     .testTarget(
       name: "HomeTests",
       dependencies: ["Home"]),
---
+++ Projects/GithubClone/Sources/AppMain.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+@main
+struct AppMain: App {
+  var body: some Scene {
+    WindowGroup {
+      VStack {
+        Spacer()
+        Text("AppMain")
+          .foregroundStyle(Color.blue)
+        Spacer()
+      }
+    }
+  }
+}
