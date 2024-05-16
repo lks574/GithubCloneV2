4c952e178585d7dedb6b3368027e6a9962025735
kyungseok.lee <lks574@naver.com>
2024-05-16 14:26:40 +0900

    feat: SwiftPackage 추가 /  Swiftgen 추가

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "GithubCloneV2",
+  products: [
+    .library(
+      name: "GithubCloneV2",
+      targets: ["GithubCloneV2"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/airbnb/swift", from: "1.0.7")
+  ],
+  targets: [
+    .target(
+      name: "GithubCloneV2"),
+    .testTarget(
+      name: "GithubCloneV2Tests",
+      dependencies: ["GithubCloneV2"]),
+  ]
+)
