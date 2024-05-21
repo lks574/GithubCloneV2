3f666df465b1b3db7f1378fa5db05bd4c5939803
kyungseok.lee <lks574@naver.com>
2024-05-21 17:23:46 +0900

    feat: User 기본 화면 구성

--- Modules/Feature/Home/Sources/Home/Page/UserDetail/UserDetailPage.swift
+++ Modules/Feature/Home/Sources/Home/Page/UserDetail/UserDetailPage.swift
@@ -14,13 +14,108 @@ struct UserDetailPage {
 
 // MARK: View
 
+extension UserDetailPage {
+
+  @ViewBuilder
+  private func userInfoView(user: GithubEntity.Users.User.Response) -> some View {
+    VStack(alignment: .leading, spacing: 8) {
+      HStack(spacing: 8) {
+        AsyncImage(url: URL(string: user.avatarURL)) { phase in
+          phase.image?.resizable()
+        }
+        .frame(width: 50, height: 50)
+
+        VStack(alignment: .leading, spacing: 4) {
+          Text(user.login)
+            .font(.system(size: 16, weight: .bold))
+            .foregroundStyle(.black)
+          Text(user.name)
+            .font(.system(size: 14, weight: .regular))
+            .foregroundStyle(.black)
+        }
+      }
+
+      HStack(spacing: 8) {
+        Image(systemName: "building")
+        Text(user.company)
+          .font(.system(size: 14, weight: .regular))
+          .foregroundStyle(.black)
+      }
+      HStack(spacing: 8) {
+        Image(systemName: "person")
+        Text("\(user.followers) 팔로워")
+          .font(.system(size: 14, weight: .regular))
+          .foregroundStyle(.black)
+        Text("\(user.following) 팔로잉")
+          .font(.system(size: 14, weight: .regular))
+          .foregroundStyle(.black)
+      }
+    }
+    .frame(maxWidth: .infinity, alignment: .leading)
+  }
+
+  @ViewBuilder
+  private func menuView(user: GithubEntity.Users.User.Response) -> some View {
+    VStack(alignment: .leading, spacing: .zero) {
+      Button(action: { }) {
+        HStack(spacing: 8) {
+          Image(systemName: "text.book.closed")
+            .foregroundStyle(Color.black)
+          Text("리포지토리")
+            .font(.system(size: 14, weight: .regular))
+            .foregroundStyle(.black)
+          Spacer()
+          Text("\(user.publicRepos)")
+            .font(.system(size: 14, weight: .regular))
+            .foregroundStyle(Color.black)
+          Image(systemName: "chevron.right")
+            .resizable()
+            .frame(width: 5, height: 10)
+            .foregroundStyle(Color.black)
+        }
+        .padding(8)
+      }
+
+      Rectangle()
+        .fill(Color.gray)
+        .frame(height: 0.5)
+        .frame(maxWidth: .infinity)
+        .padding(.leading, 30)
+
+      Button(action: { }) {
+        HStack(spacing: 8) {
+          Image(systemName: "globe")
+            .foregroundStyle(Color.black)
+          Text("URL")
+            .font(.system(size: 14, weight: .regular))
+            .foregroundStyle(.black)
+          Spacer()
+          Text(user.htmlURL)
+            .font(.system(size: 14, weight: .regular))
+            .foregroundStyle(Color.black)
+          Image(systemName: "chevron.right")
+            .resizable()
+            .frame(width: 5, height: 10)
+            .foregroundStyle(Color.black)
+        }
+        .padding(8)
+      }
+    }
+    .frame(maxWidth: .infinity, alignment: .leading)
+  }
+}
+
 extension UserDetailPage: View {
   var body: some View {
-    VStack(spacing: .zero) {
-      Text("UserDetailPage")
-      Text(store.userResponse?.company ?? "")
+    VStack(spacing: 16) {
+      if let user = store.userResponse {
+        userInfoView(user: user)
+        menuView(user: user)
+      }
       Spacer()
     }
+
+    .padding(16)
     .navigationBarTitleDisplayMode(.inline)
     .onAppear {
       send(.onLoad)
